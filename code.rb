0f78659e749eb02602dcffe90a79a327ac1c6493
WarHatch <staskeviciuskarolis@gmail.com>
2018-04-23 15:55:40 +0300

    simple health management

--- code.rb
+++ code.rb
@@ -1,35 +1,38 @@
 
 class Player
-    @wall_behind = nil
-    
-    def empty_action(warrior, backward)
-        if warrior.health < 20 && warrior.health >= @health
-          warrior.rest!
-        else
-          warrior.walk!(:backward)
-        end
-    end
-    
-    def play_turn(warrior)
-      # cool code goes here
-      unless @wall_behind
-        if warrior.feel(:backward).empty?
-          empty_action(warrior, true)
-        elsif warrior.feel(:backward).captive?
-          warrior.rescue!(:backward)
-        elsif !warrior.feel(:backward).wall?
-          @wall_behind = true;
-        end
+@wall_behind = nil
+
+def empty_action(warrior, backward)
+  if warrior.health < 20 && warrior.health >= @health
+      warrior.rest!
+  else
+      if backward == true
+      warrior.walk!(:backward)
       else
-        if warrior.feel.empty?
-          empty_action(warrior, false)
-        elsif warrior.feel.captive?
-          warrior.rescue!
-        else 
-          warrior.attack!
-        end
+      warrior.walk!
       end
-      @health = warrior.health
-    end
   end
-    
+end
+
+def play_turn(warrior)
+  if !@wall_behind
+    if warrior.feel(:backward).empty?
+      empty_action(warrior, true)
+    elsif warrior.feel(:backward).captive?
+      warrior.rescue!(:backward)
+    elsif !warrior.feel(:backward).wall?
+      @wall_behind = true
+    end
+    else
+    if warrior.feel.empty?
+      empty_action(warrior, false)
+    elsif warrior.feel.captive?
+      warrior.rescue!
+    else
+      warrior.attack!
+    end
+    end
+    @health = warrior.health
+end
+
+end
